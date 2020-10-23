68d3da1952e97b708adb05ca32e34856c1db4145
teleskop150750 <teleskop150750@gmail.com>
2020-10-23 14:38:46 +0300

    first commit

---
+++ js-padding.js
@@ -0,0 +1,18 @@
+export default () => ({
+  addPadding() {
+    const elements = document.querySelectorAll('.js-scroll');
+    elements.forEach((item) => {
+      const el = item;
+      const { paddingRight } = getComputedStyle(item);
+      el.style.paddingRight = `${parseFloat(paddingRight) + window.innerWidth - document.documentElement.clientWidth}px`;
+    });
+  },
+  removePadding() {
+    const elements = document.querySelectorAll('.js-scroll');
+    elements.forEach((item) => {
+      const el = item;
+      const { paddingRight } = getComputedStyle(item);
+      el.style.paddingRight = `${parseFloat(paddingRight) - (window.innerWidth - document.documentElement.clientWidth)}px`;
+    });
+  },
+});
